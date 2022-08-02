e9fee64af078ea662f3df47de114b26f921edd2d
bryllim <bryllim@gmail.com>
2022-08-02 20:14:45 +0800

    added 404 styling

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,5 +1,10 @@
 const NoPage = () => {
-    return <h1>404: Page not Found!</h1>;
+    return(
+      <div className="text-center jumbotron mt-5">
+        <h1 className="display-1 fw-bold">404</h1>
+        <p className="lead">Page not found</p>
+      </div>
+    )
   };
   
   export default NoPage;
