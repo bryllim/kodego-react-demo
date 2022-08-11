f0b465c870d029252a54dab760f1235f7d66f7ce
bryllim <bryllim@gmail.com>
2022-08-11 20:16:21 +0800

    useEffect examples and scss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import "./Home.scss";
+
 const Home = () => {
 
+  const [count, setCount] = useState(0);
+
+  const options = { timeZone: 'Asia/Manila', timeZoneName: 'short' };
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    setTimeout(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+  }, [currentDate]);
+
   return (
-    <div className="text-center">
-      <h1 className="mt-5">Welcome to a Blog Website</h1>
-      <p>This is a simple blog website that I created to learn React.js</p>
+    <div className="centerLine">
+      <h1 className="mt-5 textRed">Welcome to a Blog Website {currentDate.toLocaleTimeString('en-SG', options)} </h1>
+      <p className="boldFont">This is a simple blog website that I created to learn React.js</p>
       <img src="https://kodego.ph/_nuxt/img/join_asset.9a7edbb.png"></img>
     </div>
   )
