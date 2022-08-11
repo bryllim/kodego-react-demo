26d840c222688bb0a06ba99885b757e7cdd1a3ba
raymondgamiao <royalrfgum@gmail.com>
2022-08-11 20:36:11 +0800

    change home h1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,9 @@ import { useEffect, useState } from "react";
 import "./Home.scss";
 
 const Home = () => {
-
   const [count, setCount] = useState(0);
 
-  const options = { timeZone: 'Asia/Manila', timeZoneName: 'short' };
+  const options = { timeZone: "Asia/Manila", timeZoneName: "short" };
   const [currentDate, setCurrentDate] = useState(new Date());
 
   useEffect(() => {
@@ -16,11 +15,17 @@ const Home = () => {
 
   return (
     <div className="centerLine">
-      <h1 className="mt-5 textRed">Welcome to a Blog Website {currentDate.toLocaleTimeString('en-SG', options)} </h1>
-      <p className="boldFont">This is a simple blog website that I created to learn React.js</p>
+      <h1 className="mt-5 textRed">
+        Welcome to a Blog Website{" "}
+        {currentDate.toLocaleTimeString("en-SG", options)}{" "}
+      </h1>
+      <h2>Mondi was here</h2>
+      <p className="boldFont">
+        This is a simple blog website that I created to learn React.js
+      </p>
       <img src="https://kodego.ph/_nuxt/img/join_asset.9a7edbb.png"></img>
     </div>
-  )
+  );
 };
 
 export default Home;
